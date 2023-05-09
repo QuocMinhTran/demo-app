d7c8b03c2ae2683bef529fee0e8803b2702f614c
Minh Tran <minh.tran@knowit.se>
2023-05-09 10:56:52 +0200

    deploy with AWS EC2

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Github Actions
+          Learn Github Actions with AWS deployment
         </a>
       </header>
     </div>
